0cb18cdd9ae1c887f594c4f32b51b4f69dd30b92
zfletch <zfletch2@gmail.com>
2018-12-12 09:05:43 -0500

    Updates

---
+++ precache-manifest.f813b15904adebea73ea36d9157635fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0cc558e135b516e775c6",
+    "url": "/treebank-template/static/js/runtime~main.0cc558e1.js"
+  },
+  {
+    "revision": "c32f0630261e08580def",
+    "url": "/treebank-template/static/js/main.c32f0630.chunk.js"
+  },
+  {
+    "revision": "de621f6c7e6ca9c4e612",
+    "url": "/treebank-template/static/js/1.de621f6c.chunk.js"
+  },
+  {
+    "revision": "c32f0630261e08580def",
+    "url": "/treebank-template/static/css/main.d828d012.chunk.css"
+  },
+  {
+    "revision": "de621f6c7e6ca9c4e612",
+    "url": "/treebank-template/static/css/1.af6459e0.chunk.css"
+  },
+  {
+    "revision": "3dceda0d7ea631a7e380d669b595ce73",
+    "url": "/treebank-template/index.html"
+  }
+];
