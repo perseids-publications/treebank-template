bdbf67984c082a81571d47ec248f8b57b7679979
zfletch <zfletch2@gmail.com>
2019-06-06 16:33:17 -0400

    Updates

---
+++ precache-manifest.23534b69e7c7d9ee5cca1190d60339d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3231e83f5dcd40ff01835a36d6a8fd70",
+    "url": "/treebank-template/index.html"
+  },
+  {
+    "revision": "655c614efbbaf9367eaf",
+    "url": "/treebank-template/static/css/2.ec3f0427.chunk.css"
+  },
+  {
+    "revision": "e5eb7ac73c87df33595e",
+    "url": "/treebank-template/static/css/main.a730fb68.chunk.css"
+  },
+  {
+    "revision": "655c614efbbaf9367eaf",
+    "url": "/treebank-template/static/js/2.21d3e77f.chunk.js"
+  },
+  {
+    "revision": "e5eb7ac73c87df33595e",
+    "url": "/treebank-template/static/js/main.c23bc304.chunk.js"
+  },
+  {
+    "revision": "50fcfcce07a2ef801b31",
+    "url": "/treebank-template/static/js/runtime~main.34f28866.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/treebank-template/precache-manifest.4ae7e64fb000143d7e6cfeedaee90b38.js"
+  "/treebank-template/precache-manifest.23534b69e7c7d9ee5cca1190d60339d8.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/treebank-template/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/treebank-template/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
