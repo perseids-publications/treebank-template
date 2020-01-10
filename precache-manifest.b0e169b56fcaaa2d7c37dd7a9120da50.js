89a540c108964d3d0af776fb26f9a6434668ea0d
zfletch <zfletch2@gmail.com>
2020-01-10 12:59:32 -0500

    Updates

---
+++ precache-manifest.b0e169b56fcaaa2d7c37dd7a9120da50.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74de3c3f8110a4aaf53bd04862bb7902",
+    "url": "/treebank-template/index.html"
+  },
+  {
+    "revision": "29c3fcf490e5a58864e6",
+    "url": "/treebank-template/static/css/2.8dbc3e9b.chunk.css"
+  },
+  {
+    "revision": "c8eb5ee45d7731193d2b",
+    "url": "/treebank-template/static/css/main.c6a36044.chunk.css"
+  },
+  {
+    "revision": "29c3fcf490e5a58864e6",
+    "url": "/treebank-template/static/js/2.2f026b8e.chunk.js"
+  },
+  {
+    "revision": "76d7a8da27b1246d0ec7305dafc313ce",
+    "url": "/treebank-template/static/js/2.2f026b8e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c8eb5ee45d7731193d2b",
+    "url": "/treebank-template/static/js/main.224f1396.chunk.js"
+  },
+  {
+    "revision": "6da5cfca3c7f6f206524",
+    "url": "/treebank-template/static/js/runtime-main.a8b99599.js"
+  }
+]);
