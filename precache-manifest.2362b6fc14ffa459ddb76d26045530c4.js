dc35eb483506ecc5e74cf71be3d19b7e97f5f36a
Zachary Fletcher (via Travis CI) <zfletch@users.noreply.github.com>
2020-04-21 18:55:43 +0000

    Deploy perseids-publications/treebank-template to github.com/perseids-publications/treebank-template.git:gh-pages

---
+++ precache-manifest.2362b6fc14ffa459ddb76d26045530c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ecd7c58b0e36d033fa0de49c0679bde",
+    "url": "/treebank-template/index.html"
+  },
+  {
+    "revision": "7fbc9c43808e9125c1ce",
+    "url": "/treebank-template/static/css/2.0121959d.chunk.css"
+  },
+  {
+    "revision": "504415f2ad1e81ce0ad6",
+    "url": "/treebank-template/static/css/main.bd98509e.chunk.css"
+  },
+  {
+    "revision": "7fbc9c43808e9125c1ce",
+    "url": "/treebank-template/static/js/2.7c1a267b.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "/treebank-template/static/js/2.7c1a267b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "504415f2ad1e81ce0ad6",
+    "url": "/treebank-template/static/js/main.7119716b.chunk.js"
+  },
+  {
+    "revision": "e4cbd26b1ada89f155c2",
+    "url": "/treebank-template/static/js/runtime-main.aab001f8.js"
+  }
+]);
