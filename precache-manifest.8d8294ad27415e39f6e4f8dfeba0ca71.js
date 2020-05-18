bd0c1c9051982568a2b6ce8e0f4ac1da0d6c8aec
zfletch (via Travis CI) <zfletch2@gmail.com>
2020-05-18 13:37:12 +0000

    Deploy perseids-publications/treebank-template to github.com/perseids-publications/treebank-template.git:gh-pages

--- precache-manifest.8d8294ad27415e39f6e4f8dfeba0ca71.js
+++ precache-manifest.8d8294ad27415e39f6e4f8dfeba0ca71.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "dcd5c53c51cffc8065e0e17bfd26e122",
+    "revision": "46de92257a0c02c5f37eb6af7084c45b",
     "url": "/treebank-template/index.html"
   },
   {
-    "revision": "b4b32a76bd2edc75be7e",
+    "revision": "530e5646b8a3081372a0",
     "url": "/treebank-template/static/css/2.354f031a.chunk.css"
   },
   {
-    "revision": "78b215e16ead0f379b57",
+    "revision": "fd78d2c3f69a15e0cbdc",
     "url": "/treebank-template/static/css/main.bd98509e.chunk.css"
   },
   {
-    "revision": "b4b32a76bd2edc75be7e",
-    "url": "/treebank-template/static/js/2.2ba2d319.chunk.js"
+    "revision": "530e5646b8a3081372a0",
+    "url": "/treebank-template/static/js/2.1a30801d.chunk.js"
   },
   {
     "revision": "40046c021c0025c71b5ec67323b92a85",
-    "url": "/treebank-template/static/js/2.2ba2d319.chunk.js.LICENSE.txt"
+    "url": "/treebank-template/static/js/2.1a30801d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "78b215e16ead0f379b57",
-    "url": "/treebank-template/static/js/main.3283d5f8.chunk.js"
+    "revision": "fd78d2c3f69a15e0cbdc",
+    "url": "/treebank-template/static/js/main.fbe0bb32.chunk.js"
   },
   {
     "revision": "fc82e1a45c5889b39b98",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/treebank-template/precache-manifest.a64112d5b31c0f6ede6ee9183a9bae99.js"
+  "/treebank-template/precache-manifest.8d8294ad27415e39f6e4f8dfeba0ca71.js"
 );
 
 self.addEventListener('message', (event) => {
