3d5632b5be7a897a2984334474a7ad8934613b46
zfletch <zfletch@users.noreply.github.com>
2020-08-28 13:24:56 +0000

    Deploying to gh-pages from  @ 77f1784aacc48511b438535b2be8da3d5307c09f 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/treebank-template/precache-manifest.7743a55e74595b0c56cf84a3ec8c879b.js"
+  "/treebank-template/precache-manifest.55c883b20cfc4e2aa7858dcf41d6c5cb.js"
 );
 
 self.addEventListener('message', (event) => {
