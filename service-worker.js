93ae16d663c0c72a64435389910864ebc76cbe59
zfletch <zfletch2@gmail.com>
2021-01-12 11:00:46 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/treebank-template/precache-manifest.a2ebe82fe355173d9102f2637eac37ee.js"
+  "/treebank-template/precache-manifest.ce66be7dde52d0ccd00a67b23656d2a5.js"
 );
 
 self.addEventListener('message', (event) => {
