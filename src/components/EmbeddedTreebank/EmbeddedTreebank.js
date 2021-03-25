8e8efb9785ddf9e6c9b3c250f1f311d85cfb8bf9
zfletch <zfletch2@gmail.com>
2021-03-25 16:02:37 -0400

    replace embedded arethusa with treebank-react

--- src/components/EmbeddedTreebank/EmbeddedTreebank.js
+++ src/components/EmbeddedTreebank/EmbeddedTreebank.js
@@ -1,60 +1,103 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import {
+  Treebank as TB,
+  Sentence,
+  Text,
+  Graph,
+  PartOfSpeech,
+} from 'treebank-react';
+import fetch from 'cross-fetch';
 
 import { publicationMatchType, locationType } from '../../lib/types';
 
 import styles from './EmbeddedTreebank.module.css';
 
-import ArethusaWrapper from '../ArethusaWrapper';
-import TreebankStyles from '../TreebankStyles';
 import TreebankService from '../TreebankService';
 
 import { parse } from '../../lib/params';
 
+const renderLoading = () => (
+  <div>
+    Loading...
+  </div>
+);
+
+const renderTreebank = (loadedXml, publication, chunk, highlight) => (
+  <div className={styles.treebankContainer}>
+    <TB treebank={loadedXml}>
+      <Sentence
+        id={chunk}
+        highlight={highlight || []}
+      >
+        <div className={styles.text}>
+          <Text />
+        </div>
+        <div className={styles.graph}>
+          <Graph />
+        </div>
+        <PartOfSpeech />
+      </Sentence>
+    </TB>
+    <div className={styles.links}>
+      <p>
+        <a href={`${process.env.PUBLIC_URL}/${publication}/${chunk}`} target="_blank" rel="noopener noreferrer">
+          Credits and more information
+        </a>
+      </p>
+    </div>
+  </div>
+);
+
 class EmbeddedTreebank extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loadedXml: false,
+    };
+
+    this.additionalArgs = this.additionalArgs.bind(this);
+  }
+
   componentDidMount() {
-    this.renderArethusa();
+    const { xml } = this.props;
+
+    fetch(`${process.env.PUBLIC_URL}/xml/${xml}`)
+      .then((response) => response.text())
+      .then((loadedXml) => {
+        this.setState({ loadedXml });
+      });
   }
 
-  componentDidUpdate() {
-    this.renderArethusa();
-  }
+  additionalArgs() {
+    const { location: { search } } = this.props;
 
-  renderArethusa() {
-    const {
-      xml,
-      match: { params: { chunk } },
-      arethusa: { render },
-      location: { search },
-    } = this.props;
-    const additionalArgs = parse(search);
-
-    render(xml, chunk, additionalArgs);
+    return parse(search);
   }
 
   render() {
-    const { match, arethusa } = this.props;
+    const { match } = this.props;
     const { params: { publication, chunk } } = match;
+    const fullQuery = this.additionalArgs();
+
+    const { loadedXml } = this.state;
+    const { w: highlight } = fullQuery;
+
+    const component = loadedXml
+      ? renderTreebank(loadedXml, publication, chunk, highlight)
+      : renderLoading();
 
     return (
       <>
-        <div className="__artsa">
-          <div id="treebank_container" className={styles.treebankContainer} />
-        </div>
-        <div className={styles.links}>
-          <a href={`${process.env.PUBLIC_URL}/${publication}/${chunk}`} target="_blank" rel="noopener noreferrer">
-            Credits and more information
-          </a>
-        </div>
-        <TreebankStyles />
-        <TreebankService arethusa={arethusa} />
+        {component}
+        <TreebankService loaded={!!loadedXml} />
       </>
     );
   }
 }
 
 EmbeddedTreebank.propTypes = {
-  arethusa: PropTypes.instanceOf(ArethusaWrapper).isRequired,
   match: publicationMatchType.isRequired,
   location: locationType.isRequired,
   xml: PropTypes.string.isRequired,
