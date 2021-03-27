c360389cea4b9aee1ef58036baf68b758c39c385
zfletch <zfletch2@gmail.com>
2021-03-26 21:21:31 -0400

    update tests for treebank-react

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,8 +1,17 @@
-// This function is required by Alpheios messaging
+// This function is required by Alpheios Messaging
 // which uses the https://github.com/uuidjs/uuid package
 // that relies on `window.crypto.getRandomValues`
 global.crypto = {
-  getRandomValues: array => array.map(() => Math.random()),
+  getRandomValues: (array) => array.map(() => Math.random()),
 };
 
+// This is used in tests that rely on the callback functionality
+// in Treebank React's `<Sentence>` component. When the sentence is
+// loaded, it normally calls the given callback with the sentence XML
+// converted to JSON. If this variable is set, then the sentence mock
+// calls the callback with the value.
+global.treebankCallbackValue = false;
+
+// Necessary for testing parts of the code that rely on the
+// public URL.
 process.env.PUBLIC_URL = 'https://www.example.com';
